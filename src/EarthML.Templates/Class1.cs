9af51f4323791aa032976187ac645ae718320a4e
Poul Kjeldager Sørensen <pks@s-innovations.net>
2018-03-18 14:34:35 +0100

    small example of templates with razor

---
+++ src/EarthML.Templates/Class1.cs
@@ -0,0 +1,194 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.ObjectPool;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace EarthML.Templates
+{
+    public class EmailReportGenerator
+    {
+        public string GenerateReport(int userData1, int userData2)
+        {
+            return "A user specific report based on values " + userData1 + " and " + userData2;
+        }
+    }
+
+
+    public class EmailViewModel
+    {
+        public string UserName { get; set; }
+
+        public string SenderName { get; set; }
+
+        public int UserData1 { get; set; }
+
+        public int UserData2 { get; set; }
+    }
+    public class EmailTemplateService
+    {
+        private readonly IServiceScopeFactory scopeFactory;
+
+        public EmailTemplateService(string customApplicationBasePath = null)
+        {
+
+            scopeFactory = InitializeServices(customApplicationBasePath);
+
+        }
+        public static IServiceScopeFactory InitializeServices(string customApplicationBasePath = null)
+        {
+            // Initialize the necessary services
+            var services = new ServiceCollection();
+            ConfigureDefaultServices(services, customApplicationBasePath);
+
+            // Add a custom service that is used in the view.
+            services.AddSingleton<EmailReportGenerator>();
+
+            var serviceProvider = services.BuildServiceProvider();
+            return serviceProvider.GetRequiredService<IServiceScopeFactory>();
+        }
+
+        public Task<string> RenderViewAsync()
+        {
+            using (var serviceScope = scopeFactory.CreateScope())
+            {
+                var helper = serviceScope.ServiceProvider.GetRequiredService<RazorViewToStringRenderer>();
+
+                var model = new EmailViewModel
+                {
+                    UserName = "User",
+                    SenderName = "Sender",
+                    UserData1 = 1,
+                    UserData2 = 2
+                };
+
+                return helper.RenderViewToStringAsync("Templates/WelcomeMail.cshtml", model);
+            }
+        }
+
+        private static void ConfigureDefaultServices(IServiceCollection services, string customApplicationBasePath)
+        {
+            string applicationName;
+            IFileProvider fileProvider;
+            if (!string.IsNullOrEmpty(customApplicationBasePath))
+            {
+                applicationName = Path.GetFileName(customApplicationBasePath);
+                fileProvider = new PhysicalFileProvider(customApplicationBasePath);
+            }
+            else
+            {
+                applicationName = Assembly.GetEntryAssembly().GetName().Name;
+                fileProvider = new PhysicalFileProvider(Directory.GetCurrentDirectory());
+            }
+
+            services.AddSingleton<IHostingEnvironment>(new HostingEnvironment
+            {
+                ApplicationName = applicationName,
+                WebRootFileProvider = fileProvider,
+            });
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+
+                options.FileProviders.Clear();
+                options.FileProviders.Add(fileProvider);
+            });
+            var diagnosticSource = new DiagnosticListener("Microsoft.AspNetCore");
+            services.AddSingleton<ObjectPoolProvider, DefaultObjectPoolProvider>();
+            services.AddSingleton<DiagnosticSource>(diagnosticSource);
+            services.AddLogging();
+            services.AddMvc().AddRazorPagesOptions(o => { o.RootDirectory = "/templates"; }).WithRazorPagesRoot("/templates");
+            services.AddTransient<RazorViewToStringRenderer>();
+        }
+    }
+    public class RazorViewToStringRenderer
+    {
+        private IRazorViewEngine _viewEngine;
+        private ITempDataProvider _tempDataProvider;
+        private IServiceProvider _serviceProvider;
+
+        public RazorViewToStringRenderer(
+            IRazorViewEngine viewEngine,
+            ITempDataProvider tempDataProvider,
+            IServiceProvider serviceProvider)
+        {
+            _viewEngine = viewEngine;
+            _tempDataProvider = tempDataProvider;
+            _serviceProvider = serviceProvider;
+        }
+
+        public async Task<string> RenderViewToStringAsync<TModel>(string viewName, TModel model)
+        {
+            var actionContext = GetActionContext();
+            var view = FindView(actionContext, viewName);
+
+            using (var output = new StringWriter())
+            {
+                var a = new ViewDataDictionary<TModel>(
+                        metadataProvider: new EmptyModelMetadataProvider(),
+                        modelState: new ModelStateDictionary())
+                {
+                    Model = model,
+
+                };
+               
+                var viewContext = new ViewContext(
+                    actionContext,
+                    view,a
+                    ,
+                    new TempDataDictionary(
+                        actionContext.HttpContext,
+                        _tempDataProvider),
+                    output,
+                    new HtmlHelperOptions());
+
+                await view.RenderAsync(viewContext);
+
+                return output.ToString();
+            }
+        }
+
+        private IView FindView(ActionContext actionContext, string viewName)
+        {
+            var getViewResult = _viewEngine.GetView(executingFilePath: null, viewPath: viewName, isMainPage: true);
+            if (getViewResult.Success)
+            {
+                return getViewResult.View;
+            }
+
+            var findViewResult = _viewEngine.FindView(actionContext, viewName, isMainPage: true);
+            if (findViewResult.Success)
+            {
+                return findViewResult.View;
+            }
+
+            var searchedLocations = getViewResult.SearchedLocations.Concat(findViewResult.SearchedLocations);
+            var errorMessage = string.Join(
+                Environment.NewLine,
+                new[] { $"Unable to find view '{viewName}'. The following locations were searched:" }.Concat(searchedLocations)); ;
+
+            throw new InvalidOperationException(errorMessage);
+        }
+
+        private ActionContext GetActionContext()
+        {
+            var httpContext = new DefaultHttpContext();
+            httpContext.RequestServices = _serviceProvider;
+            return new ActionContext(httpContext, new RouteData(), new ActionDescriptor());
+        }
+    }
+}
